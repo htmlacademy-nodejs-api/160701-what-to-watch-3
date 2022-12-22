27965ac18328b129a274cddb0b6a7fe8b83e47e7
Дмитрий <shcheklein.dev@gmail.com>
2022-12-22 09:46:27 +0300

    Добавит метод parseCommand менеджеру. WIP

--- src/cli-command/cli-application.ts
+++ src/cli-command/cli-application.ts
@@ -1,5 +1,8 @@
 import { CliCommandInterface } from './cli-command.interface.js';
 
+type ParsedCommand = {
+  [key: string]: string[];
+};
 export default class CliApplication {
   private commands: { [propertyName: string]: CliCommandInterface } = {};
 
@@ -9,5 +12,21 @@ export default class CliApplication {
 
       return acc;
     }, this.commands);
+  }
+
+  private parseCommand(cliArguments: string[]): ParsedCommand {
+    const parsedCommand: ParsedCommand = {};
+    let command = '';
+
+    return cliArguments.reduce((acc, item) => {
+      if (item.startsWith('--')) {
+        acc[item] = [];
+        command = item;
+      } else if (command && item) {
+        acc[command].push(item);
+      }
+
+      return acc;
+    }, parsedCommand);
   }
 }
