b1249510155cf89851824e61ae7f235451851ede
Дмитрий <shcheklein.dev@gmail.com>
2022-12-23 08:49:35 +0300

    Fix: HelpCommand

--- src/cli-command/help-command.ts
+++ src/cli-command/help-command.ts
@@ -1,6 +1,6 @@
 import { CliCommandInterface } from './cli-command.interface.js';
 
-export default class VersionCommand implements CliCommandInterface {
+export default class HelpCommand implements CliCommandInterface {
   public readonly name = '--help';
 
   public async execute(): Promise<void> {
