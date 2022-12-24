dc990e209cca9e11ddc45c635670d72d74051e68
Дмитрий <shcheklein.dev@gmail.com>
2022-12-25 00:09:25 +0300

    Добавит и зарегистрирует команду --import

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,12 @@
 import CliApplication from './app/cli-application.js';
 import VersionCommand from './cli-command/version-command.js';
 import HelpCommand from './cli-command/help-command.js';
+import ImportCommand from './cli-command/import-command.js';
 
 const myManager = new CliApplication();
-myManager.registerCommands([new HelpCommand(), new VersionCommand()]);
+myManager.registerCommands([
+  new HelpCommand(),
+  new VersionCommand(),
+  new ImportCommand(),
+]);
 myManager.processCommand(process.argv);
