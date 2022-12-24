7045c765a8cf86c6d85730f0edaa0e2821bc3ded
Дмитрий <shcheklein.dev@gmail.com>
2022-12-25 00:45:50 +0300

    Fix: вывод ошибки в ImportCommand

--- src/cli-command/import-command.ts
+++ src/cli-command/import-command.ts
@@ -4,16 +4,21 @@ import { CliCommandInterface } from './cli-command.interface.js';
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
   public execute(filename: string): void {
-    const fileReader = new TSVFileReader(filename.trim());
     try {
+      if (!filename || filename.trim() === '') {
+        throw new Error('Не передан путь до файла');
+      }
+
+      const fileReader = new TSVFileReader(filename.trim());
       fileReader.read();
+
       console.log(fileReader.toArray());
     } catch (error) {
       if (!(error instanceof Error)) {
         throw error;
       }
       console.log(
-        `Не удалось импортировать данные из файла по причине: «${err.message}»`
+        `Не удалось импортировать данные из файла по причине: «${error.message}»`
       );
     }
   }
