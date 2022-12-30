f752828854e2e3b0168288c198e50aec292b1eb9
Dmitry <sheklein.dima@gmail.com>
2022-12-30 10:34:52 +0300

    Добавит тип MockData.

---
+++ src/types/mock-data.type.ts
@@ -0,0 +1,17 @@
+export type MockData = {
+  names: string[];
+  descriptions: string[];
+  created: string[];
+  genres: string[];
+  previewVideoLinks: string[];
+  videoLink: string[];
+  starring: string[];
+  director: string[];
+  posterImage: string[];
+  backgroundImage: string[];
+  backgroundColor: string[];
+  firstname: string[];
+  email: string[];
+  avatarPath: string[];
+  passwords: string[];
+};
