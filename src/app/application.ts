a2c0061461e2f53dd62220ff83804872a8959117
Дмитрий <shcheklein.dev@gmail.com>
2023-01-05 09:28:59 +0300

    Добавит ConfigService в Application.

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,11 +1,14 @@
+import ConfigService from '../common/config/config.service.js';
 import { LoggerInterface } from '../common/logger/logger.interface.js';
 
 export default class Application {
-  constructor(private logger: LoggerInterface) {
+  constructor(private logger: LoggerInterface, private config: ConfigService) {
     this.logger = logger;
+    this.config = config;
   }
 
   public async init() {
     this.logger.info('App init');
+    this.logger.info(`Get value from env $PORT ${this.config.get('PORT')}`);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import Application from './app/application.js';
+import ConfigService from './common/config/config.service.js';
 import LoggerServise from './common/logger/logger.service.js';
 
 const logger = new LoggerServise();
+const config = new ConfigService(logger);
 
-const application = new Application(logger);
+const application = new Application(logger, config);
 application.init();
