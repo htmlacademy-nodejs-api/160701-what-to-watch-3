5fd9ecbdc2f5661356d5689dbd74c1c7cbe2b89c
Дмитрий <shcheklein.dev@gmail.com>
2023-01-05 23:32:34 +0300

    Внедрит зависимость в ConfigService.

--- src/common/config/config.service.ts
+++ src/common/config/config.service.ts
@@ -2,11 +2,14 @@ import { config } from 'dotenv';
 import { ConfigInterface } from './config.interface.js';
 import { LoggerInterface } from '../logger/logger.interface.js';
 import { ConfigSchema, configSchema } from './config.schema.js';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/component.types.js';
 
+@injectable()
 export default class ConfigService implements ConfigInterface {
   private config: ConfigSchema;
 
-  constructor(private logger: LoggerInterface) {
+  constructor(@inject(Component.LoggerInterface) logger: LoggerInterface) {
     this.logger = logger;
 
     const parsedOutput = config();
