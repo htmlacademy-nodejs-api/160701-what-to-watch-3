5a0932ee500e9aabb3ecc846df889f84579f9ad2
Дмитрий <shcheklein.dev@gmail.com>
2023-01-15 12:52:29 +0300

    Добавит уникальность для поля email. WIP

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -8,7 +8,10 @@ export interface UserDocument extends User, mongoose.Document {
 
 const userSchema = new mongoose.Schema(
   {
-    email: String,
+    email: {
+      type: String,
+      unique: true,
+    },
     avatarPath: String,
     firstname: String,
     lastname: String,
