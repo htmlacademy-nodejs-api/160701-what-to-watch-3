5aed956e6420c8b934f4f6e16186a99a6c0e55cb
Дмитрий <shcheklein.dev@gmail.com>
2023-01-15 11:57:05 +0300

    Добавит класс DatabaseService.

---
+++ src/common/database-client/database.service.ts
@@ -0,0 +1,20 @@
+import { inject } from 'inversify';
+import { DatabaseInterface } from './database.interface.js';
+import { Component } from '../../types/component.types.js';
+import { LoggerInterface } from '../logger/logger.interface.js';
+import mongoose from 'mongoose';
+
+export default class DatabaseService implements DatabaseInterface {
+  constructor(@inject(Component.LoggerInterface) private logger: LoggerInterface) {}
+
+  public async connect(uri: string): Promise<void> {
+    this.logger.info('Try to connect');
+    await mongoose.connect(uri);
+    this.logger.info('Database connection established.');
+  }
+
+  public async disconnect(): Promise<void> {
+    await mongoose.disconnect();
+    this.logger.info('Database connection closed');
+  }
+}
