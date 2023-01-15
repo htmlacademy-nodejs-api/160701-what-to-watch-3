9133ee68ac09d4c4bb5c7073b6310c9461a8cb06
Дмитрий <shcheklein.dev@gmail.com>
2023-01-15 13:32:36 +0300

    Рефакторит модель до класса. WIP

---
+++ src/modules/user/user.entity.ts
@@ -0,0 +1,9 @@
+import { User } from '../../types/user.type.js';
+
+export class UserEntity implements User {
+  public email!: string;
+  public avatarPath!: string;
+  public firstname!: string;
+  public lastname!: string;
+  public password!: string;
+}
