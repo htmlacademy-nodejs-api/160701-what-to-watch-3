d3734f7af9599f688aaf475c8214bccb6bdf1144
Дмитрий <shcheklein.dev@gmail.com>
2023-01-15 14:54:26 +0300

    Разобьёт общий контейнер. WIP

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import { Container } from 'inversify';
 import 'reflect-metadata';
 import { Component } from './types/component.types.js';
 import Application from './app/application.js';
+import { applicationContainer } from './app/application.container.js';
+
+const mainContainer = Container.merge(applicationContainer);
 
 async function bootstrap() {
-  const mainContainer = new Container();
   const application = mainContainer.get<Application>(Component.Application);
-
   await application.init();
 }
 
