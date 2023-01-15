e5487622a9bf680c776fbad549cc548b0d850c05
Дмитрий <shcheklein.dev@gmail.com>
2023-01-15 15:12:36 +0300

    Добавит внедрение модели в зависимости. WIP

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,20 +1,23 @@
-import { DocumentType } from '@typegoose/typegoose';
+import { DocumentType, types } from '@typegoose/typegoose';
 import CreateUserDto from './dto/create-user.dto.js';
 import { UserServiceInterface } from './user-service.interface.js';
-import { UserEntity, UserModel } from './user.entity.js';
+import { UserEntity } from './user.entity.js';
 import { inject, injectable } from 'inversify';
 import { Component } from '../../types/component.types.js';
 import { LoggerInterface } from '../../common/logger/logger.interface.js';
 
 @injectable()
 export default class UserService implements UserServiceInterface {
-  constructor(@inject(Component.LoggerInterface) private logger: LoggerInterface) {}
+  constructor(
+    @inject(Component.LoggerInterface) private logger: LoggerInterface,
+    @inject(Component.UserModel) private readonly userModel: types.ModelType<UserEntity>,
+  ) {}
 
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    const result = await UserModel.create(user);
+    const result = await this.userModel.create(user);
     this.logger.info(`New user created: ${user.email}`);
 
     return result;
