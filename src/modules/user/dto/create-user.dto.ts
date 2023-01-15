3b4b4f3efd39d40b801d808c0472308c55913a7d
Дмитрий <shcheklein.dev@gmail.com>
2023-01-15 14:17:42 +0300

    Добавит класс CreateUserDTO.

---
+++ src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export default class CreateUserDto {
+  public email!: string;
+  public avatarPath!: string;
+  public firsname!: string;
+  public lastname!: string;
+  public password!: string;
+}
