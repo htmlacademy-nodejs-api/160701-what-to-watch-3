004fcd7989ab30efac4617235657a1c99ba7925f
Дмитрий <shcheklein.dev@gmail.com>
2023-01-15 20:55:17 +0300

    Добавит CreateFilmDTO.

---
+++ src/modules/film/dto/create-film.dto.ts
@@ -0,0 +1,20 @@
+import { Genres } from '../../../types/film.type.js';
+import { User } from '../../../types/user.type.js';
+
+export default class CreateFilmDto {
+  public name!: string;
+  public posterImage!: string;
+  public backgroundImage!: string;
+  public backgroundColor!: string;
+  public videoLink!: string;
+  public previewVideoLink!: string;
+  public description!: string;
+  public rating!: number;
+  public director!: string;
+  public starring!: string[];
+  public runTime!: number;
+  public genre!: Genres;
+  public created!: Date;
+  public released!: number;
+  public user!: User;
+}
