e83554fbf87a019b0b21971e5568959c52bf1626
Дмитрий <shcheklein.dev@gmail.com>
2023-01-15 15:06:02 +0300

    Добавит внедрение зависимостей в UserService. WIP

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import 'reflect-metadata';
 import { Component } from './types/component.types.js';
 import Application from './app/application.js';
 import { applicationContainer } from './app/application.container.js';
+import { userContainer } from './modules/user/user.container.js';
 
-const mainContainer = Container.merge(applicationContainer);
+const mainContainer = Container.merge(applicationContainer, userContainer);
 
 async function bootstrap() {
   const application = mainContainer.get<Application>(Component.Application);
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,12 +2,21 @@ import { DocumentType } from '@typegoose/typegoose';
 import CreateUserDto from './dto/create-user.dto.js';
 import { UserServiceInterface } from './user-service.interface.js';
 import { UserEntity, UserModel } from './user.entity.js';
+import { inject, injectable } from 'inversify';
+import { Component } from '../../types/component.types.js';
+import { LoggerInterface } from '../../common/logger/logger.interface.js';
 
+@injectable()
 export default class UserService implements UserServiceInterface {
+  constructor(@inject(Component.LoggerInterface) private logger: LoggerInterface) {}
+
   public async create(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>> {
     const user = new UserEntity(dto);
     user.setPassword(dto.password, salt);
 
-    return UserModel.create(user);
+    const result = await UserModel.create(user);
+    this.logger.info(`New user created: ${user.email}`);
+
+    return result;
   }
 }
