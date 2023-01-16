8d41fd6ac5101eff6094a35e8b7a9a99ec5b50a4
Дмитрий <shcheklein.dev@gmail.com>
2023-01-16 09:28:20 +0300

    Добавит filmContainer

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { Component } from './types/component.types.js';
 import Application from './app/application.js';
 import { applicationContainer } from './app/application.container.js';
 import { userContainer } from './modules/user/user.container.js';
+import { filmContainer } from './modules/film/film.container.js';
 
 const mainContainer = Container.merge(applicationContainer, userContainer, filmContainer);
 
