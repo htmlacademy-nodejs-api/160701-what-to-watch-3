825058d18b8552dfe5eecad3a03728181b5a6226
Дмитрий <shcheklein.dev@gmail.com>
2023-02-12 13:21:46 +0300

    Расширяет интерфейс FilmService

--- src/modules/film/film-service.interface.ts
+++ src/modules/film/film-service.interface.ts
@@ -1,8 +1,18 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { FilmEntity } from './film.entity.js';
 import CreateFilmDto from './dto/create-film.dto.js';
+import UpdateFilmDto from './dto/update-film.dto.js';
 
 export interface FilmServiceInterface {
   create(dto: CreateFilmDto): Promise<DocumentType<FilmEntity>>;
   findById(filmID: string): Promise<DocumentType<FilmEntity> | null>;
+  find(count?: number): Promise<DocumentType<FilmEntity>[]>;
+  deleteById(filmID: string): Promise<DocumentType<FilmEntity> | null>;
+  updateById(filmID: string, dto: UpdateFilmDto): Promise<DocumentType<FilmEntity> | null>;
+  findByGenre(genre: string, count?: number): Promise<DocumentType<FilmEntity>[]>;
+  incCommentCount(filmID: string): Promise<DocumentType<FilmEntity> | null>;
+  findPromo(): Promise<DocumentType<FilmEntity>>;
+  findFavorite(): Promise<DocumentType<FilmEntity>[]>;
+  changeFavorite(filmId: string): Promise<DocumentType<FilmEntity>>;
+  exists(documentId: string): Promise<boolean>;
 }
